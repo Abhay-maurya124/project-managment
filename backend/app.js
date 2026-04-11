7f831ba755418f722791b41e8875e4e7a7d7d1d1
Abhay maurya <abhaymaurya6013@gmail.com>
2026-04-11 18:26:20 +0530

    somting

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,42 @@
-import cookieParser from "cookie-parser";import { config } from "dotenv";import express from "express";import cors from "cors";import { errorMiddleware } from "./middleware/error.js";import { router } from "./router/userRouter.js";import { adminrouter } from "./router/adminroutes.js";import { Studentrouter } from "./router/studentRoutes.js";import { notificationrouter } from "./router/notificationroutes.js";import { fileURLToPath } from "url";import path from "path";import fs from "fs";import { projectRoutes } from "./router/projectsroutes.js";import { deadlinerouter } from "./router/deadlineroutes.js";import { Teacherrouter } from "./router/teacherrroutes.js";config();const __filename = fileURLToPath(import.meta.url);const __dirname = path.dirname(__filename);const app = express();app.use(  cors({    origin: [process.env.FRONT_URL],    methods: ["GET", "PUT", "DELETE", "POST"],    credentials: true,  }),);const uploadDir = path.join(__dirname, "uploads");const tempDir = path.join(__dirname, "temp");if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir, { recursive: true });app.use(cookieParser());app.use(express.json());app.use(express.urlencoded({ extended: true }));app.use("/user", router);app.use("/admin", adminrouter);app.use("/student", Studentrouter);app.use("/teacher", Teacherrouter);app.use("/notification", notificationrouter);app.use("/project", projectRoutes);app.use("/deadline", deadlinerouter);app.use(errorMiddleware);export default app;
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from "./middleware/error.js";
+import { router } from "./router/userRouter.js";
+import { adminrouter } from "./router/adminroutes.js";
+import { Studentrouter } from "./router/studentRoutes.js";
+import { notificationrouter } from "./router/notificationroutes.js";
+import { projectRoutes } from "./router/projectsroutes.js";
+import { deadlinerouter } from "./router/deadlineroutes.js";
+import { Teacherrouter } from "./router/teacherrroutes.js";
+
+config();
+
+const app = express();
+app.use(
+  cors({
+    origin: [process.env.FRONT_URL],
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", router);
+app.use("/admin", adminrouter);
+app.use("/student", Studentrouter);
+app.use("/teacher", Teacherrouter);
+app.use("/notification", notificationrouter);
+app.use("/project", projectRoutes);
+app.use("/deadline", deadlinerouter);
+app.get("/", (req, res) => {
+  res.send("Server is Running Smoothly");
+});
+
+app.use(errorMiddleware);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-import app from "./app.js";import { connectDB } from "./config/db.js";connectDB();const server = app.listen(process.env.PORT, () => {  try {    console.log(`connected to port ${process.env.PORT}`);  } catch (error) {    console.log(      `connection error to port ${process.env.PORT} with error -: ${error}`,    );  }});//error handlingprocess.on("unhandledRejection", (err) => {  console.log(`unhandled Rejection ${err.message}`);  server.close(() => process.exit(1));});process.on("uncaughtException", (err) => {  console.log(`uncaught Exception ${err.message}`);  process.exit(1);});
+import app from "./app.js";
+import { connectDB } from "./config/db.js";
+connectDB();
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  try {
+    console.log(`connected to port ${PORT}`);
+  } catch (error) {
+    console.log(
+      `connection error to port ${PORT} with error -: ${error}`,
+    );
+  }
+});
+//error handling
+process.on("unhandledRejection", (err) => {
+  console.log(`unhandled Rejection ${err.message}`);
+  server.close(() => process.exit(1));
+});
+process.on("uncaughtException", (err) => {
+  console.log(`uncaught Exception ${err.message}`);
+  process.exit(1);
+});
